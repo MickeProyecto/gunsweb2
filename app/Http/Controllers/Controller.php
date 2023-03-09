920619d07fc1ab6658f3c4cfa73444167513f5bc
VictorMoisesHonorCastro <73166294+VictorMoisesHonorCastro@users.noreply.github.com>
2023-03-09 16:57:05 +0100

    first commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+    public function signup(Request $request)
+    {
+        $request->validate([
+            'photo' => 'required| unique:users',
+            'name' => 'required',
+            'lastname' => 'required',
+            'password' => 'required',
+            'email' => 'required | email | unique:users',
+            'phone' => 'required | unique:users',
+            'dni' => 'required | unique:users',
+            'role',
+        ]);
+
+        $user = new User();
+        $user->photo = $request->photo;
+        $user->name = $request->name;
+        $user->lastname = $request->lastname;
+        $user->password = Hash::make($request->password);
+        $user->email = $request->email;
+        $user->phone = $request->phone;
+        $user->dni = $request->dni;
+
+        $existing_users_count = User::count();
+        if ($existing_users_count == 0) {
+            $user->rol = 'admin';
+        } else {
+            $user->rol = 'cliente';
+        }
+
+        $user->save();
+        Auth::login($user);
+        return response()->json([
+            "status" => 1,
+            'message' => 'Successfully created user!',
+            "value" => $user
+        ]);
+    }
+    public function login(Request $request)
+    {
+        $request->validate([
+            "dni" => "required",
+            "password" => "required"
+        ]);
+        $user = user::where("dni", "=", $request->dni)->first();
+        if (isset($user->id)) {
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken("auth_token")->plainTextToken;
+                return response()->json([
+                    $user,
+                    $token
+                ]);
+            } else {
+                return response()->json(
+                    "Password_bad"
+                );
+            }
+        } else {
+            return response()->json(
+                "Username_bad"
+            );
+        }
+    }
+    public function userprofile()
+    {
+        return response()->json([
+            "status" => 0,
+            "message" => "Acerca del perfil del usuario",
+            "data" => auth()->user()
+        ]);
+    }
+    public function update(Request $request, $id)
+    {
+        $user_id = auth()->user()->id;
+        if (user::where(["id" => $user_id])->exists()) {
+            $update = user::find($user_id);
+            $update->password = Hash::make($request->password);
+            $update->save();
+            return response()->json([
+                "status" => 1,
+                "message" => "Actualizado correctamente",
+            ]);
+        } else {
+            return response()->json([
+                "status" => 1,
+                "message" => "No se pùdo actucalizar",
+            ]);
+        }
+    }
+    // public function logout()
+    // {
+    //     auth()->user()->tokens()->delete();
+    //     return response()->json([
+    //         "status" => 1,
+    //         "message" => "cierre de session",
+    //     ]);
+    // }
+}
---
+++ routes/api.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});*/
+Route::post("signup", [Controller::class, "signup"]);
+Route::post("login", [Controller::class, "login"]);
+
+Route::group(['middleware' => ["auth:sanctum"]], function () {
+    Route::get("userprofile", [Controller::class, "userprofile"]);
+    Route::get("logout", [Controller::class, "logout"]);
+    Route::put("update/{id}", [Controller::class, "update"]);
+});
+
+/*
+Route::get("indexu", [UserController::class, "indexu"]);
+//Route::post("createu",[UserController::class,"createu"]);
+//Route::get("readu",[UserController::class,"readu"]);
+Route::put("updateu/{id}", [UserController::class, "updateu"]);
+Route::delete("deleteu/{id}", [UserController::class, "deleteu"]);
+
+Route::get("indexpu", [PuntoentregaController::class, "indexpu"]);
+Route::post("createpu", [PuntoentregaController::class, "createpu"]);
+//Route::get("readpu",[PuntoentregaController::class,"readpu"]);
+Route::put("updatepu/{id}", [PuntoentregaController::class, "updatepu"]);
+Route::delete("deletepu/{id}", [PuntoentregaController::class, "deletepu"]);
+
+Route::get("indexpe", [PedidosController::class, "indexpe"]);
+Route::post("createpe", [PedidosController::class, "createpe"]);
+//Route::get("readpe",[PedidosController::class,"readpe"]);
+Route::put("updatepe/{id}", [PedidosController::class, "updatepe"]);
+Route::delete("deletepe/{id}", [PedidosController::class, "deletepe"]);
+
+Route::get("indexm", [MaterialController::class, "indexm"]);
+Route::post("createm", [MaterialController::class, "createm"]);
+//Route::get("readm",[MaterialController::class,"readm"]);
+Route::put("updatem/{id}", [MaterialController::class, "updatem"]);
+Route::delete("deletem/{id}", [MaterialController::class, "deletem"]);
+
+Route::get("indexc", [CategoriaController::class, "indexc"]);
+Route::post("createc", [CategoriaController::class, "createc"]);
+//Route::get("readc",[CategoriaController::class,"readc"]);
+Route::put("updatec/{id}", [CategoriaController::class, "updatec"]);
+Route::delete("deletec/{id}", [CategoriaController::class, "deletec"]);
+*/
